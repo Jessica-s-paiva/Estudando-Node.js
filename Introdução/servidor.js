6cbef0c35169b9b16ef95d1d7085257c9b7de3e3
Jessica-s-Paiva <jessicapaivamvp@gmail.com>
2023-12-11 18:09:48 -0300

    Servidor e App de uso de memória RAM

--- Introdução/servidor.js
+++ Introdução/servidor.js
@@ -1,1 +1,7 @@
 const http  = require('http');
+const host  = "http://localhost";
+const port = 3000;
+
+http.createServer((req, res) => {
+    req.end(<h1>Working</h1>)
+}).listen(port, () => {console.log(`Server is running in ${host}:${port}`);});
