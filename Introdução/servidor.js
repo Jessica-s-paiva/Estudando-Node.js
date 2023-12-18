d9c855bb980da01a8f1d89cd812ec323d034c22c
Jessica-s-Paiva <jessicapaivamvp@gmail.com>
2023-12-18 14:01:11 -0300

    atualizando

--- Introdução/servidor.js
+++ Introdução/servidor.js
@@ -1,7 +1,15 @@
 const http  = require('http');
 const host  = "http://localhost";
 const port = 3000;
+const stats = require("./pcRunUsage");
 
 http.createServer((req, res) => {
-    req.end(<h1>Working</h1>)
+
+    let url = req.url;
+    if (url == "/") {
+        res.end("<h1>Working</h1>"); 
+    } else if (url == host) {
+        res.end(JSON.stringify(stats, null, 2));
+    }
+    
 }).listen(port, () => {console.log(`Server is running in ${host}:${port}`);});
